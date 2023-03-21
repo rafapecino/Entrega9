91abe2f76b634a5f30b289bc15ed4614ed1846d9
rafapecino <rpecbon@myuax.com>
2023-03-21 20:28:42 +0100

    Initial commit

---
+++ src/main/java/org/example/INodo.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public interface Nodo {
+    //Interface de un nodo
+    public Nodo sumar(Nodo otro);
+    public Nodo multiplicar(Nodo otro);
+    public Nodo restar(Nodo otro);
+    public Nodo dividir(Nodo otro);
+}
